50f027582a159c088aa071ec8d3c2be7479bb74e
khushi3030 <71833910+khushi3030@users.noreply.github.com>
2023-07-12 01:56:50 +0530

    Add files via upload

---
+++ temprunner.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 12 01:56:08 2023
+
+@author: KHUSHI CHAUHAN
+"""
+
+print("center array")
+    # print(center_array)
+    # print("----------------------")
